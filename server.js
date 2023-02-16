e8cff214c5694e96741a5f3a1941353814e5228a
kaminari0206 <y1281820@gmail.com>
2023-02-15 21:28:59 -0500

    .

--- server.js
+++ server.js
@@ -4,14 +4,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 // Express APIs
 const api = require('./routes/auth.routes')
-mongoose
-  .connect('mongodb://127.0.0.1:27017/auth')
-  .then((x) => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch((err) => {
-    console.error('Error connecting to mongo', err.reason)
-  })
+
 // Express settings
 const app = express()
 app.use(bodyParser.json())
